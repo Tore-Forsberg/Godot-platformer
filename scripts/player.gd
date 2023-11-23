3860375b240d23022f10df11525c85dafd8e5e37
Tore-Forsberg <tore.forsberg@elev.ga.ntig.se>
2023-11-23 15:28:47 +0100

    Update movement calculation

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
+var velocity = Vector2.ZERO # The player's movement vector.
 var screen_size # Size of the game window.
 
 # Called when the node enters the scene tree for the first time.
@@ -11,21 +12,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = horizontalMovement()
-	
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+	velocity = calculateWalkVelocity(velocity)
 
-func horizontalMovement():
-	var velocity = Vector2.ZERO # The player's movement vector.
-	
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
+
+	if velocity.x != 0:
 		# Gets the AnimatedSprite2D node and plays the walk animation
 		$AnimatedSprite2D.play("walk")
 		
@@ -34,4 +26,16 @@ func horizontalMovement():
 		# Gets the AnimatedSprite2D node and stops the walk animation
 		$AnimatedSprite2D.play("idle")
 	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+func calculateWalkVelocity(velocity):
+	if not Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+		velocity.x = 0
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	
 	return velocity
+
