9beff51bab4fcd9c5be3ebc3396fa5eee72759cb
Tore-Forsberg <tore.forsberg@elev.ga.ntig.se>
2023-11-24 15:56:37 +0100

    Update animations and add acceleration

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
+@export var speed = 200 # How fast the player will move (pixels/sec).
+@export var acceleration = 20
+@export var deacceleration = 10
 @export var jump_force = 800
-@export var gravity = 80
+@export var gravity = 30
 var screen_size # Size of the game window.
 @onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
 
@@ -16,20 +18,29 @@ func _ready():
 func _physics_process(delta):	
 	if !is_on_floor():
 		velocity.y += gravity
+		animated_sprite.play("idle")
 		if velocity.y > 1000:
 			velocity.y = 1000
 	else:
 		if Input.is_action_pressed("jump"):
 			velocity.y = -jump_force
-			animated_sprite.play("idle")
 	
 	var direction =  Input.get_axis("move_left", "move_right")
-	velocity.x = speed * direction
+	velocity.x = min(velocity.x + (acceleration * direction), speed)
+	if velocity.x > 0 or velocity.x < 0:
+		if direction != 0:
+			velocity.x -= deacceleration*direction
+		else:
+			if velocity.x > 0:
+				velocity.x -= deacceleration
+			else:
+				velocity.x += deacceleration
 	
 
 	if velocity.x != 0:
 		# Gets the AnimatedSprite2D node and plays the walk animation
-		animated_sprite.play("walk")
+		if is_on_floor():
+			animated_sprite.play("walk")
 		
 		animated_sprite.flip_h = velocity.x < 0
 	else:
