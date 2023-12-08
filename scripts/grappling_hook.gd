2242ee07b70a182892afbf3007330ecd8cec6886
Tore-Forsberg <tore.forsberg@elev.ga.ntig.se>
2023-12-08 09:58:45 +0100

    Update how to get player in grappling_hook.gd

--- scripts/grappling_hook.gd
+++ scripts/grappling_hook.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@onready var player : CharacterBody2D = $".."
+@onready var player_sprite : AnimatedSprite2D = $"../AnimatedSprite2D"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,12 +12,6 @@ func _ready():
 func _process(delta):
 	var mouse_position = get_global_mouse_position()
 	look_at(mouse_position)
-	
-	var player = get_parent()
-	var player_sprite
-	for child in player.get_children():
-		if child is AnimatedSprite2D:
-			player_sprite = child
 	
 	if mouse_position.x > player.position.x:
 		player_sprite.flip_h = false
