523e0d87ecabb139c4e415f94530a8ecc8a16f9f
Tore Forsberg <erotfor@gmail.com>
2024-01-28 14:45:55 +0100

    Fix error where blast does not work in corners

--- scripts/magnetic_blast.gd
+++ scripts/magnetic_blast.gd
@@ -4,6 +4,8 @@ extends Area2D
 @onready var explosion_timer : Timer = $ExplosionTimer
 @onready var active_timer : Timer = $ActiveTimer
 @onready var projectile_sprite: Sprite2D = $Sprite2D
+@onready var projectile_collision: CollisionShape2D = $ProjectileCollisionShape
+@onready var explosion_collision: CollisionShape2D = $ExplosionCollisionShape
 
 
 @export var speed = 2000
@@ -20,6 +22,8 @@ var is_hitting_player: bool
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_as_top_level(true)
+	explosion_collision.disabled = true
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -45,24 +49,34 @@ func _on_active_timer_timeout():
 
 
 func explode():
-	#queue_free()
+	# Starts an instance of the explosion particles
 	var _particle = explosion_particle.instantiate()
 	_particle.position = global_position
 	_particle.rotation = global_rotation
 	_particle.emitting = true
-	scale = Vector2(12, 12)
-	projectile_sprite.visible = false
+	
+	# Adds the particle to the scene
 	get_tree().current_scene.add_child(_particle)
+	
+	# Hides the sprite
+	projectile_sprite.visible = false
+	
+	# Disables the projectile collision and enables the explosion collision
+	projectile_collision.disabled = true
+	explosion_collision.disabled = false
+	
 	is_explosion_active = true
 	active_timer.start()
-	
 
 
 func magnetic_blast_explosion():
+	var x_velocity
+	var y_velocity
 	if player != null:
-		var x_velocity = (player.position.x - global_position.x)*magnetic_blast_knockback
-		var y_velocity = (player.position.y - global_position.y)*magnetic_blast_knockback
+		x_velocity = (player.position.x - global_position.x)*magnetic_blast_knockback
+		y_velocity = (player.position.y - global_position.y)*magnetic_blast_knockback
 		player.velocity = Vector2(x_velocity, y_velocity)
+	print("X: ", x_velocity, "Y: ", y_velocity)
 
 
 func _on_body_entered(body):
