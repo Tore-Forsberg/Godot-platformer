bd0c30c895a10aca760c74fc1b461131e6a61b55
Tore-Forsberg <tore.forsberg@elev.ga.ntig.se>
2024-02-02 11:33:13 +0100

    Add basic enemy

---
+++ scripts/garbage_bag.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+
+@onready var left_raycast : RayCast2D = $LeftRayCast
+@onready var right_raycast : RayCast2D = $RightRayCast
+
+@export var movement_range = 1000
+@export var speed = 200
+
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	velocity.x = speed
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if left_raycast.is_colliding() or right_raycast.is_colliding():
+		velocity.x *= -1
+
+	move_and_slide()
+
+
+func death():
+	queue_free()
+
+
+func _on_top_checker_body_entered(body):
+	if body is Player:
+		death()
+
+
+func _on_side_checker_body_entered(body):
+	if body is Player:
+		body.death()
