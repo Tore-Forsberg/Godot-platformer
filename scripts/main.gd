69e2259072be8add68eac9618361a3bfc6cd6988
Tore Forsberg <erotfor@gmail.com>
2024-02-22 13:47:13 +0100

    Remove world scene and replace with main scene

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,8 +1,10 @@
-extends Node2D
+extends Node
 
 @export var available_levels : Array [LevelData]
 
+@onready var node_2d = $Node2D
+
 
 func _ready() -> void:
-	LevelManager.main_scene = self
+	LevelManager.main_scene = node_2d
 	LevelManager.levels = available_levels
